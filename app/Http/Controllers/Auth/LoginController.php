85339a57d1db6d5767996baed88069c4d367ca28
nakagawa-tomohide <nakagawa.tmhd@gmail.com>
2024-11-29 09:26:17 +0900

    ログアウト後ログイン画面に遷移するように修正

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -36,5 +37,15 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * ログアウト後ログイン画面に遷移
+     *
+     * @return void
+     */
+    protected function loggedOut()
+    {
+        return redirect('/login');
     }
 }
