0a95ecf942bd3672587570c7825260f6a91e5b5c
nakagawa-tomohide <nakagawa.tmhd@gmail.com>
2024-12-26 00:12:09 +0900

    change https develop

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         \Illuminate\Support\Facades\Schema::defaultStringLength(191);
-        if (\App()->environment(['production'])) {
+        if (\App()->environment(['production']) || \App()->environment(['develop'])) {
             \URL::forceScheme('https');
         }
     }
